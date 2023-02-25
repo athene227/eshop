d5bc3b619043133238a528aea8aa1a14f844df2a
Tomas-Dean <athene227@gmail.com>
2023-02-25 06:41:46 +0100

    welcome page changed, navbar and layout

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,24 +1,24 @@
-<nav class="fixed w-full h-[76px] flex bg-gray-800 text-white z-20">
+<nav class="fixed w-full h-[76px] flex bg-white z-20">
     <div class="w-[90%] px-[30px] m-auto">
         <div class="flex h-full">
             <div class="flex w-1/3 h-full items-center">
                 <a href="/" class="">
-                    <img src="/images/logo-inverted.png" class="w-[77px] h-[27.5px]" />
+                    <img src="/images/logo.png" class="w-[100px] h-[40px]" />
                 </a>
             </div>
-            <div class="flex flex-row-reverse w-2/3 h-full items-center">
-                <ul>
+            <div class="flex flex-row-reverse w-2/3 items-center">
+                <ul class="text-[#1D80BB] font-semibold">
                     <li class="relative inline-block m-0">
-                        <a class="p-[15px] text-sm font-normal">LIGHTING</a>
+                        <a href="#" class="p-[15px] text-md">SHOP</a>
                     </li>
                     <li class="relative inline-block m-0">
-                        <a class="p-[15px] text-sm font-normal">BATHROOM</a>
+                        <a href="#" class="p-[15px] text-md">SERVICES</a>
                     </li>
                     <li class="relative inline-block m-0">
-                        <a class="p-[15px] text-sm font-normal">FURNITURE</a>
+                        <a href="#" class="p-[15px] text-md">ABOUT US</a>
                     </li>
                     <li class="relative inline-block m-0">
-                        <a class="p-[15px] text-sm font-normal">BEST-SELLERS</a>
+                        <a href="#" class="p-[15px] text-md">CONTACT</a>
                     </li>
                     <li class="relative inline-block m-0">
                         <a href="/login" class="px-[7.5px] py-[15px]">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,132 +1,46 @@
 @extends('layouts.app')
 
-@push('style')
-<style>
-    .carousel-open:checked + .carousel-item {
-        position: static;
-        opacity: 100;
-    }
-    .carousel-item {
-        -webkit-transition: opacity 0.6s ease-out;
-        transition: opacity 0.6s ease-out;
-    }
-    #carousel-1:checked ~ .control-1,
-    #carousel-2:checked ~ .control-2,
-    #carousel-3:checked ~ .control-3 {
-        display: block;
-    }
-    .carousel-indicators {
-        list-style: none;
-        margin: 0;
-        padding: 0;
-        position: absolute;
-        bottom: 2%;
-        left: 0;
-        right: 0;
-        text-align: center;
-        z-index: 10;
-    }
-    #carousel-1:checked ~ .control-1 ~ .carousel-indicators li:nth-child(1) .carousel-bullet,
-    #carousel-2:checked ~ .control-2 ~ .carousel-indicators li:nth-child(2) .carousel-bullet,
-    #carousel-3:checked ~ .control-3 ~ .carousel-indicators li:nth-child(3) .carousel-bullet {
-        color: #2b6cb0;  /*Set to match the Tailwind colour you want the active one to be */
-    }
-</style>
-@endpush
-
 @section('content')
-    <div class="absolute inset-0 z-0 w-full h-full flex bg-[url('/images/back.png')] bg-cover">
-        <div class="absolute w-full h-full pt-[76px] mx-auto opacity-50">
-            <div class="slide relative h-full">
-                <div class="grid grid-cols-2 lg:grid-cols-3 place-content-center">
-                    <div>
-                        <img src="/images/tile1.png" />
-                    </div>
-                    <div>
-                        <img src="/images/tile2.png" />
-                    </div>
-                    <div>
-                        <img src="/images/tile3.png" />
-                    </div>
-                    <div>
-                        <img src="/images/tile4.png" />
-                    </div>
-                    <div>
-                        <img src="/images/tile5.png" />
-                    </div>
-                    <div>
-                        <img src="/images/tile6.png" />
-                    </div>
-                </div>
-                <div class="absolute bottom-0 w-full px-5 py-3 bg-black/40 text-center text-white">Flower One Caption</div>
+    <!-- Welcome Begin -->
+    <div class="z-0 w-full min-w-[500px] px-[50px] lg:px-[5%] grid grid-cols-1 lg:grid-cols-2 bg-gradient-to-tr from-[#30B1BF] to-[#2B1D6E] gap-[50px] lg:gap-[5%]">
+        <div class="mt-[100px] lg:mt-[35%] text-white">
+            <h1 class="text-[50px] whitespace-pre-wrap">Welcome to our shop</h1>
+            <p class="mt-4 text-2xl">
+               We greet every client as an opportunity to grow our community. Get your dream goods from our store.
+            </p>
+            <button class="bg-white mt-8 rounded-full py-4 px-10 flex items-center gap-2 hover:bg-gray-100 text-gray-900">
+                <p class="text-2xl inline">Go shopping</p>
+                <i class="fa fa-chevron-right mt-1"></i>
+            </button>
+        </div>
+        <div class="flex py-[50px]">
+            <div class="mx-auto grid grid-cols-2 gap-4">
+                <a href="#" class="overflow-clip bg-white">
+                    <img src="/images/tile1.png" class="hover:opacity-70 transition ease-in-out hover:scale-110">
+                </a>
+                <a href="#" class="overflow-clip bg-white">
+                    <img src="/images/tile2.png" class="hover:opacity-70 transition ease-in-out hover:scale-110">
+                </a>
+                <a href="#" class="overflow-clip bg-white">
+                    <img src="/images/tile3.png" class="hover:opacity-70 transition ease-in-out hover:scale-110">
+                </a>
+                <a href="#" class="overflow-clip bg-white">
+                    <img src="/images/tile4.png" class="hover:opacity-70 transition ease-in-out hover:scale-110">
+                </a>
+                <a href="#" class="overflow-clip bg-white">
+                    <img src="/images/tile5.png" class="hover:opacity-70 transition ease-in-out hover:scale-110">
+                </a>
+                <a href="#" class="overflow-clip bg-white">
+                    <img src="/images/tile6.png" class="hover:opacity-70 transition ease-in-out hover:scale-110">
+                </a>
             </div>
-
-            <div class="slide relative h-full">
-                <img class="w-full h-full object-cover"
-                    src="https://www.kindacode.com/wp-content/uploads/2022/07/flower-2.jpeg">
-                <div class="absolute bottom-0 w-full px-5 py-3 bg-black/40 text-center text-white">Flower Two Caption</div>
-            </div>
-
-            <div class="slide relative h-full">
-                <img class="w-full h-full object-cover"
-                    src="https://www.kindacode.com/wp-content/uploads/2022/07/flower-3.jpeg">
-                <div class="absolute bottom-0 w-full px-5 py-3 bg-black/40 text-center text-white">Flower Three Caption
-                </div>
-            </div>
-
-            <!-- The previous button -->
-            <a class="absolute left-0 top-1/2 p-4 -translate-y-1/2 bg-black/30 hover:bg-black/50 text-white hover:text-amber-500 cursor-pointer"
-                onclick="moveSlide(-1)">❮</a>
-
-            <!-- The next button -->
-            <a class="absolute right-0 top-1/2 p-4 -translate-y-1/2 bg-black/30 hover:bg-black/50 text-white hover:text-amber-500 cursor-pointer"
-                onclick="moveSlide(1)">❯</a>
-
         </div>
     </div>
+    <!-- Welcome End -->
+
+    <!-- Services Begin -->
+    <div class="w-full px-[50px]">
+
+    </div>
+    <!-- Services End -->
 @endsection
-
-@push('script')
-<script>
-    // set the default active slide to the first one
-    let slideIndex = 1;
-    showSlide(slideIndex);
-
-    // change slide with the prev/next button
-    function moveSlide(moveStep) {
-        showSlide(slideIndex += moveStep);
-    }
-
-    // change slide with the dots
-    function currentSlide(n) {
-        showSlide(slideIndex = n);
-    }
-
-    function showSlide(n) {
-        let i;
-        const slides = document.getElementsByClassName("slide");
-        const dots = document.getElementsByClassName('dot');
-
-        if (n > slides.length) { slideIndex = 1 }
-        if (n < 1) { slideIndex = slides.length }
-
-        // hide all slides
-        for (i = 0; i < slides.length; i++) {
-            slides[i].classList.add('hidden');
-        }
-
-        // remove active status from all dots
-        for (i = 0; i < dots.length; i++) {
-            dots[i].classList.remove('bg-yellow-500');
-            dots[i].classList.add('bg-green-600');
-        }
-
-        // show the active slide
-        slides[slideIndex - 1].classList.remove('hidden');
-
-        // highlight the active dot
-        dots[slideIndex - 1].classList.remove('bg-green-600');
-        dots[slideIndex - 1].classList.add('bg-yellow-500');
-    }
-</script>
-@endpush
